08a15102ff03799d198b29206d2453e7a5bf10b9
Huang Jonathan <huangjonathan@outlook.fr>
2019-05-11 17:09:23 +0200

    Pusblisher Port and Connector

--- src/publisher/PublicationsImplementationI.java
+++ src/publisher/PublicationsImplementationI.java
@@ -4,9 +4,9 @@ import message.MessageI;
 
 public interface PublicationsImplementationI {
 	
-	public void publish(MessageI m, String topic);
-	public void publish(MessageI m, String[] topics);
-	public void publish(MessageI[] ms, String topic);
-	public void publish(MessageI[] ms, String[] topics);
+	public void publish(MessageI m, String topic) throws Exception;
+	public void publish(MessageI m, String[] topics) throws Exception;
+	public void publish(MessageI[] ms, String topic) throws Exception;
+	public void publish(MessageI[] ms, String[] topics) throws Exception;
 
 }
