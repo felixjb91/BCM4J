6350bded34d45ac9d344ef5595fac8fb32a18001
Huang Jonathan <huangjonathan@outlook.fr>
2019-05-11 13:31:48 +0200

    TimeStamp

--- src/message/TimeStamp.java
+++ src/message/TimeStamp.java
@@ -1,5 +1,38 @@
 package message;
 
-public class TimeStamp {
+import java.io.Serializable;
+
+public class TimeStamp implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private long time;
+	private String timestamper;
+	
+	public TimeStamp(long time, String timestamper) {
+		this.time = time;
+		this.timestamper = timestamper;
+	}
+	
+	public boolean isInitialised() {
+		if((this.time != -1)&&(this.timestamper != null)) return true;
+		else return false;
+	}
+	
+	public long getTime() {
+		return time;
+	}
+	public void setTime(long time) {
+		this.time = time;
+	}
+	public String getTimestamper() {
+		return timestamper;
+	}
+	public void setTimestamper(String timestamper) {
+		this.timestamper = timestamper;
+	}
 
 }
