a3d587ccbe845dee59b790fb77d1ea67d074e231
Huang Jonathan <huangjonathan@outlook.fr>
2019-05-13 20:39:29 +0200

    Test unitaire Probleme: pas acces aux HashMap (messages & subcriptions) peut pas tester les methodes de PublicationI et ManagementI sur les subcriber

--- test/broker/AbstractBrokerTest.java
+++ test/broker/AbstractBrokerTest.java
@@ -4,11 +4,18 @@ package broker;
 import org.junit.Before;
 import org.junit.Test;
 
+import broker.impl.BrokerImpl;
 import broker.interfaces.ManagementI;
 import broker.interfaces.PublicationI;
+import message.MessageFilterI;
+import message.MessageI;
+
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 
 public abstract class AbstractBrokerTest {
 	
@@ -20,8 +27,10 @@ public abstract class AbstractBrokerTest {
 	public abstract void setUp() throws Exception;
 	
 	@Test
-	public void createTopicTest() {
-		
+	public void createTopicTest() throws Exception {
+		this.managementProvider.createTopic("Hacking");
+		String[] topics = {"Hacking"};
+		assertArrayEquals(topics, this.managementProvider.getTopics());
 	}
 	
 	@Test 
@@ -30,5 +39,40 @@ public abstract class AbstractBrokerTest {
 		String[] topics = {"Hacking"};
 		assertArrayEquals(topics, this.managementProvider.getTopics());
 	}
+	
+	@Test
+	public void destroyTopicTest() throws Exception {
+		this.managementProvider.createTopic("Hacking");
+		String[] topics = this.managementProvider.getTopics();
+		this.managementProvider.destroyTopic("Hacking");
+		assertFalse(Arrays.asList(topics).contains("Hacking"));		
+	}
+	
+	@Test
+	public void isTopicTest() throws Exception {
+		this.managementProvider.createTopic("Hacking");
+		assertTrue(this.managementProvider.isTopic("Hacking"));
+	}
+	
+	@Test
+	public void subscribeTest() throws Exception {
+	
+	}
+	
+	@Test
+	public void modifyFilterTest() throws Exception {
+	
+	}
+	
+	@Test
+	public void unsubscribeTest() throws Exception {
+			
+	}
+	
+	@Test
+	public void publish() throws Exception {
+		
+	}
+		
 
 }
