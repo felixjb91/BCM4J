99a94f00a749092bf1c7cbfe61ef6d6446c5aa1b
Tahar <mail@tahar-hidja.fr>
2019-05-14 01:44:18 +0200

    General Updates (See description)
    * adding the CVM class
    * PublishImpl improvement
    * Give appropriate names to publisher ports
    * Message class improvement
    * BrokerImpl improvement, but partially working

---
+++ src/CVM/CVM.java
@@ -0,0 +1,63 @@
+package CVM;
+
+import broker.impl.BrokerImpl;
+import connectors.ManagementConnector;
+import connectors.PublicationsConnector;
+import fr.sorbonne_u.components.cvm.AbstractCVM;
+import publisher.impl.PublisherImpl;
+
+public class CVM extends AbstractCVM {
+
+	protected static final String BROKER_PUB_IN = "BROKER_PUBLICATION_IN_PORT_URI";
+	protected static final String BROKER_REC_OUT = "BROKER_RECEPTION_OUT_PORT_URI";
+	protected static final String BROKER_MAN_IN = "BROKER_MANAGEMENT_IN_PORT_URI";
+	
+	protected static final String PUBLISHER_PUB_OUT = "PUBLISHER_PUBLICATION_OUT_PORT_URI";
+	protected static final String PUBLISHER_MAN_OUT = "PUBLISHER_MANAGEMENT_OUT_PORT_URI";
+	
+	protected static final String SUBSCRIBER_REC_IN = "SUBSCRIBER_RECEPTION_IN_PORT_URI";
+	protected static final String SUBSCRIBER_MAN_OUT = "SUBSCRIBER_MANAGEMENT_OUT_PORT_URI";
+
+	
+	public CVM() throws Exception {
+		super();
+	}
+
+	@Override
+	public void deploy() throws Exception {
+		
+		BrokerImpl b = new BrokerImpl(BROKER_MAN_IN, BROKER_REC_OUT, BROKER_PUB_IN);
+		PublisherImpl p = new PublisherImpl(PUBLISHER_PUB_OUT ,PUBLISHER_MAN_OUT) ;
+		//Subscriber s = new Subscriber(URI6,URI7) ;
+		
+		p.toggleTracing() ;
+		b.toggleTracing() ;
+		//s.toggleTracing() ;
+		
+		/*b.doPortConnection(
+			URI2, URI6,
+			ReceptionsConnector.class.getCanonicalName()); */
+		p.doPortConnection(
+			PUBLISHER_PUB_OUT, BROKER_PUB_IN,
+			PublicationsConnector.class.getCanonicalName());
+		p.doPortConnection(
+			PUBLISHER_MAN_OUT, BROKER_MAN_IN,
+			ManagementConnector.class.getCanonicalName());
+		/*s.doPortConnection(
+			URI7, URI3,
+			ManagementConnector.class.getCanonicalName());
+		 */
+		super.deploy();
+	}
+
+	public static void main(String[] args) {
+		try {
+			CVM cvm = new CVM() ;
+			cvm.startStandardLifeCycle(2000L) ;
+			Thread.sleep(10000L);
+			System.exit(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/publisher/impl/PublisherImpl.java
@@ -0,0 +1,127 @@
+package publisher.impl;
+
+import broker.interfaces.ManagementImplementationI;
+import broker.interfaces.PublicationsImplementationI;
+import fr.sorbonne_u.components.AbstractComponent;
+import fr.sorbonne_u.components.cvm.AbstractCVM;
+import fr.sorbonne_u.components.exceptions.ComponentShutdownException;
+import message.Message;
+import message.MessageI;
+import publisher.ports.PublicationOutbountPort;
+import publisher.ports.ManagementOutboundPort;
+
+
+public class PublisherImpl 
+extends AbstractComponent 
+implements PublicationsImplementationI, ManagementImplementationI {
+	
+	protected PublicationOutbountPort publicationOutboundPort;
+	protected ManagementOutboundPort managementOutboundPort;
+
+	public PublisherImpl(String publicationOutboundPortUri, String managementOutboundPortUri) throws Exception {
+		
+		super(1, 0);
+		
+		assert publicationOutboundPortUri != null;
+		assert managementOutboundPortUri != null;
+		// create the port that exposes the required interface
+		this.publicationOutboundPort = new PublicationOutbountPort(publicationOutboundPortUri, this);
+		this.managementOutboundPort = new ManagementOutboundPort(managementOutboundPortUri, this);
+		// add the port to the set of ports of the component
+		this.addPort(this.publicationOutboundPort);
+		this.addPort(this.managementOutboundPort);
+
+		this.publicationOutboundPort.publishPort();
+		this.managementOutboundPort.publishPort();
+
+		if (AbstractCVM.isDistributed) {
+			this.executionLog.setDirectory(System.getProperty("user.dir"));
+		} else {
+			this.executionLog.setDirectory(System.getProperty("user.home"));
+		}
+		this.tracer.setTitle("Pusblisher");
+		this.tracer.setRelativePosition(1, 1);
+	}
+
+	@Override
+	public void publish(MessageI m, String topic) throws Exception {
+		this.publicationOutboundPort.publish(m,topic);
+	}
+
+	@Override
+	public void publish(MessageI m, String[] topics) throws Exception {
+		this.publicationOutboundPort.publish(m,topics);
+
+	}
+
+	@Override
+	public void publish(MessageI[] m, String topic) throws Exception {
+		this.publicationOutboundPort.publish(m,topic);
+
+	}
+
+	@Override
+	public void publish(MessageI[] m, String[] topics) throws Exception {
+		this.publicationOutboundPort.publish(m,topics);
+	}
+
+	@Override
+	public void createTopic(String topic) throws Exception {
+		this.managementOutboundPort.createTopic(topic);	
+	}
+
+	@Override
+	public void createTopics(String[] topics) throws Exception {
+		this.managementOutboundPort.createTopics(topics);
+	}
+
+	@Override
+	public void destroyTopic(String topic) throws Exception {
+		this.managementOutboundPort.destroyTopic(topic);
+	}
+
+	@Override
+	public boolean isTopic(String topic) throws Exception {
+		return this.managementOutboundPort.isTopic(topic);
+	}
+
+	@Override
+	public String[] getTopics() throws Exception {
+		return this.managementOutboundPort.getTopics();
+	}
+
+	
+	@Override
+	public void execute() throws Exception {
+		super.execute();
+		String[] lesTopics = {"tpoic1", "tpoic2", "tpoic3"};
+		String[] lesTopics2 = {"tpoic3", "tpoic4"};
+		createTopics(lesTopics);
+		publish(new Message("hello World"), lesTopics);
+		createTopics(lesTopics2);
+		destroyTopic("topic1");
+		destroyTopic("topic4");
+	}
+	
+	@Override
+	public void	finalise() throws Exception
+	{		
+		this.doPortDisconnection(this.publicationOutboundPort.getPortURI());
+		this.doPortDisconnection(this.managementOutboundPort.getPortURI());
+		
+		super.finalise();
+	}
+	
+	@Override
+	public void shutdown() throws ComponentShutdownException {
+		try {
+			this.publicationOutboundPort.unpublishPort();
+			this.managementOutboundPort.unpublishPort();
+		} catch (Exception e) {
+			throw new ComponentShutdownException(e) ;
+		}
+		super.shutdown();
+	}
+	
+	
+}
