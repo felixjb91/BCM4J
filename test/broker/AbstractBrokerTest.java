667a5043bcdfbc45e8d1575ba3dce39312ce81c2
Huang Jonathan <huangjonathan@outlook.fr>
2019-05-14 12:12:09 +0200

    Test unitaire subscriber a fini

--- test/broker/AbstractBrokerTest.java
+++ test/broker/AbstractBrokerTest.java
@@ -4,10 +4,18 @@ package broker;
 import org.junit.Before;
 import org.junit.Test;
 
+import broker.implementation.Broker;
 import broker.interfaces.ManagementI;
 import broker.interfaces.PublicationI;
+import subscriber.implementation.Subscriber;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 
 
 public abstract class AbstractBrokerTest {
@@ -20,8 +28,10 @@ public abstract class AbstractBrokerTest {
 	public abstract void setUp() throws Exception;
 	
 	@Test
-	public void createTopicTest() {
-		
+	public void createTopicTest() throws Exception {
+		this.managementProvider.createTopic("Hacking");
+		String[] topics = {"Hacking"};
+		assertArrayEquals(topics, this.managementProvider.getTopics());
 	}
 	
 	@Test 
@@ -29,6 +39,47 @@ public abstract class AbstractBrokerTest {
 		this.managementProvider.createTopic("Hacking");
 		String[] topics = {"Hacking"};
 		assertArrayEquals(topics, this.managementProvider.getTopics());
+	}
+	
+	@Test
+	public void destroyTopicTest() throws Exception {
+		this.managementProvider.createTopic("Hacking");
+		String[] topics = this.managementProvider.getTopics();
+		this.managementProvider.destroyTopic("Hacking");
+		assertFalse(Arrays.asList(topics).contains("Hacking"));		
+	}
+	
+	@Test
+	public void isTopicTest() throws Exception {
+		this.managementProvider.createTopic("Hacking");
+		assertTrue(this.managementProvider.isTopic("Hacking"));
+	}
+	
+	@Test
+	public void subscribeTest() throws Exception {
+		Broker broker = new Broker(1, 0, null, null, null);
+		String topic = "Hello";
+		String inboundPortUri = "inboundPort";
+		broker.subscribe(topic, inboundPortUri);
+		/*Subscriber subscriber = new Subscriber(inboundPortUri,null);
+		Map<String,Set<Subscriber>> subscribers = {topic, subscriber};
+		assertArrayEquals(subscribers, broker.getSubscriber());*/
+		
+	}
+	
+	@Test
+	public void modifyFilterTest() throws Exception {
+	
+	}
+	
+	@Test
+	public void unsubscribeTest() throws Exception {
+			
+	}
+	
+	@Test
+	public void publish() throws Exception {
+		
 	}
 
 }
